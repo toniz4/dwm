17655eb8a63ca988a8b894d62f02ab222921cd6d
toniz4 <cassioavila000@gmail.com>
2020-07-02 08:51:40 -0300

    modified fonts

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh = 12; /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "scientifica:size=8", "Siji:size=8", };
+static const char *fonts[]          = { "scientifica:size=8","Misc Stlarch:size=8", "Siji:size=8","JoyPixels:size=8", };
 static const char dmenufont[]       = "scientifica:size=8";
 static const char col_bg[]          = "#1d1f21";
 static const char col_fg[]          = "#c5c8c6";
