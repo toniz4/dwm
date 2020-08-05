c39781ec33cc76c7c5e442b83e775f1f73d99af5
toniz4 <cassioavila000@gmail.com>
2020-08-05 11:55:37 -0300

    updated config

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_sel, "-sf", col_fg, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
